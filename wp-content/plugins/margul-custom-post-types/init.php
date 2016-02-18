449ba3c50fe2f5639480848fcfdd5c79be391a26
Marcus <gullbeerg@gmail.com>
2016-02-18 11:53:13 -0800

    Finalized board builder design

--- wp-content/plugins/margul-custom-post-types/init.php
+++ wp-content/plugins/margul-custom-post-types/init.php
@@ -90,6 +90,7 @@ function mg_enqueues() {
 	// Default script dependancies
 	$dependencies = array('jquery');
 
+	// If we are on the boardBuilder page then add the custom bootstrap.
 	if($post->ID == 28) {
 		wp_enqueue_style( 'bb-custom-bootstrap', MG_CSS . '/bootstrap.min.css' );
 		wp_enqueue_script( 'bb-custom-bootstrap', MG_JS . '/bootstrap.min.js', $dependencies, '1.0');
