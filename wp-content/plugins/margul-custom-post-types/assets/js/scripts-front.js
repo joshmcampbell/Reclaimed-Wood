04f9a915a7cbe861e129a96c9000b6ce9fc0588c
joshmcampbell@outlook.com <joshmcampbell@outlook.com>
2016-03-13 16:48:09 -0700

    Finished validation, additional check for postal code length

--- wp-content/plugins/margul-custom-post-types/assets/js/scripts-front.js
+++ wp-content/plugins/margul-custom-post-types/assets/js/scripts-front.js
@@ -1,10 +1,36 @@
 jQuery(document).ready(function($) {
+	var error = false;
+	$('#checkout-name').on('keypress', function(event) {
+		error = false;
+		document.getElementById("nameDiv").style.color="black";
+	});
+	$('#checkout-address').on('keypress', function(event) {
+		error = false;
+		document.getElementById("addressDiv").style.color="black";
+	});
+	$('#checkout-province').on('change', function(event) {
+		error = false;
+		document.getElementById("provinceDiv").style.color="black";
+	});
+	$('#checkout-city').on('keypress', function(event) {
+		error = false;
+		document.getElementById("cityDiv").style.color="black";
+	});
+	$('#checkout-phone').on('keypress', function(event) {
+		error = false;
+		document.getElementById("phoneDiv").style.color="black";
+	});
+	$('#checkout-postal').on('keypress', function(event) {
+		error = false;
+		document.getElementById("postalDiv").style.color="black";
+	});
+
 	$('#checkout-btn').on('click', function(event) {
-		var error = false;
 		var errorMsg = "You need to fill in your ";
 
 		if(!$('#checkout-name').val()){
 			error=true;
+			document.getElementById("nameDiv").style.color="red";
 			$('#checkout-name').addClass('has-error');
 			errorMsg += "name, ";
 		} else{
@@ -12,13 +38,15 @@ jQuery(document).ready(function($) {
 		}
 		if(!$('#checkout-address').val()){
 			error=true;
+			document.getElementById("addressDiv").style.color="red";
 			$('#checkout-address').addClass('has-error');
 			errorMsg += "address, ";
 		} else{
 			$('#checkout-address').removeClass('hass-error');
 		}
-		if(!$('#checkout-province').val()){
+		if($('#checkout-province').val() == 'df'){
 			error=true;
+			document.getElementById("provinceDiv").style.color="red";
 			$('#checkout-province').addClass('has-error');
 			errorMsg += "province, ";
 		} else{
@@ -26,6 +54,7 @@ jQuery(document).ready(function($) {
 		}
 		if(!$('#checkout-city').val()){
 			error=true;
+			document.getElementById("cityDiv").style.color="red";
 			$('#checkout-city').addClass('has-error');
 			errorMsg += "city, ";
 		} else{
@@ -33,6 +62,7 @@ jQuery(document).ready(function($) {
 		}
 		if(!$('#checkout-phone').val()){
 			error=true;
+			document.getElementById("phoneDiv").style.color="red";
 			$('#checkout-phone').addClass('has-error');
 			errorMsg += "phone number, ";
 		} else{
@@ -40,6 +70,7 @@ jQuery(document).ready(function($) {
 		}
 		if(!$('#checkout-postal').val()){
 			error=true;
+			document.getElementById("postalDiv").style.color="red";
 			$('#checkout-postal').addClass('has-error');
 			errorMsg += "postal code, ";
 		} else{
@@ -49,8 +80,13 @@ jQuery(document).ready(function($) {
 		errorMsg += "before you can move on.";
 
 		if(error == true) {
-			event.defaultPrevented;
-			alert(errorMsg);
+			event.preventDefault();
+			return false;
+			strHtml = "";
+			strHtml += "<br>";
+			strHtml += errorMsg;
+			strHtml += "<br>";
+			document.getElementById("alertDiv").append(strHtml);
 		}
 	});
 });
--- wp-content/plugins/margul-custom-post-types/views/checkout.php
+++ wp-content/plugins/margul-custom-post-types/views/checkout.php
@@ -27,11 +27,11 @@
 				<font size="1"><i>* denotes required fields.</i></font>
 			</div>
 			<br>
-				<div class="form-group order-div <?php echo (in_array('custName', $objData->errors)) ? 'has-error' : ''; ?>">
+				<div id="nameDiv" class="form-group order-div <?php echo (in_array('custName', $objData->errors)) ? 'has-error' : ''; ?>">
 					Full Name*:
 					<input type="text" id="checkout-name" name="custName" class="right alt-width" value="<?php echo ($objData->error) ? $objData->input['custName'] : ''; ?>">
 				</div>
-				<div class="form-group order-div <?php echo (in_array('custAddr', $objData->errors)) ? 'has-error' : ''; ?>">
+				<div id="addressDiv" class="form-group order-div <?php echo (in_array('custAddr', $objData->errors)) ? 'has-error' : ''; ?>">
 					Address*:
 					<input type="text" id="checkout-address" name="custAddr" class="right alt-width" value="<?php echo ($objData->error) ? $objData->input['custAddr'] : ''; ?>">
 				</div>
@@ -39,7 +39,7 @@
 					Address (line 2):
 					<input type="text" id="checkout-addr2"name="custAddr2" class="right alt-width" value="<?php echo ($objData->error) ? $objData->input['custAddr2'] : ''; ?>">
 				</div>
-				<div class="form-group order-div <?php echo (in_array('custProv', $objData->errors)) ? 'has-error' : ''; ?>">
+				<div id="provinceDiv" class="form-group order-div <?php echo (in_array('custProv', $objData->errors)) ? 'has-error' : ''; ?>">
 					Province*:
 					<select name="custProv" id="checkout-province" class="form-control board-func right alt-width" data-id="0" value="<?php echo ($objData->error) ? $objData->input['custProv'] : ''; ?>">
 			       		<option value="df">===Select Province===</option>  
@@ -55,24 +55,23 @@
 		        		<option value="nl">Newfoundland and Labrador</option>
 			        </select>
 				</div>
-				<div class="form-group order-div <?php echo (in_array('custCity', $objData->errors)) ? 'has-error' : ''; ?>">
+				<div id="cityDiv" class="form-group order-div <?php echo (in_array('custCity', $objData->errors)) ? 'has-error' : ''; ?>">
 					City*:
 					<input type="text" id="checkout-city" name="custCity"  class="right alt-width" value="<?php echo ($objData->error) ? $objData->input['custCity'] : ''; ?>">
 				</div>
-				<div class="form-group order-div <?php echo (in_array('custNum', $objData->errors)) ? 'has-error' : ''; ?>">
+				<div id="phoneDiv" class="form-group order-div <?php echo (in_array('custNum', $objData->errors)) ? 'has-error' : ''; ?>">
 					Phone Number*:
 					<input type="text" id="checkout-phone" name="custNum" class="right alt-width" value="<?php echo ($objData->error) ? $objData->input['custCustNum'] : ''; ?>">
 				</div>
-				<div class="form-group order-div <?php echo (in_array('custCode', $objData->errors)) ? 'has-error' : ''; ?>">
+				<div id="postalDiv" class="form-group order-div <?php echo (in_array('custCode', $objData->errors)) ? 'has-error' : ''; ?>">
 					Postal Code*:
-					<input type="text" id="checkout-postal" name="custCode" class="right alt-width" max="6" value="<?php echo ($objData->error) ? $objData->input['custCode'] : ''; ?>">
+					<input type="text" id="checkout-postal" name="custCode" class="right alt-width" maxlength="6" value="<?php echo ($objData->error) ? $objData->input['custCode'] : ''; ?>">
 				</div>
 				<div class="form-group">
 					<input type="hidden" id="hidden-field-boards" name="board" value="<?php json_encode($_POST['boards']); ?>"/>
 				</div>
-				<?php if($objData->error) { ?>
-                    <div class="alert alert-danger" role="alert"><strong>You seem to be missing something:</strong><br><?php echo implode('<br>', $objData->msg); ?></div>
-                <?php } ?>
+                <div class="full-width" id="alertDiv">
+                </div>
 				<div class="alt-width right">
 				<br>
 					<input type="submit" id="checkout-btn" class="btn btn-continue half-width left" value="Checkout">
